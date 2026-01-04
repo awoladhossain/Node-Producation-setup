f34b9e6858192afbf4eb8e566298a045a9d6d151
awoladhossain <awoladh04@gmail.com>
2026-01-04 14:01:31 +0600

    feat(prettier): Add the script file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,41 +1,44 @@
 // @ts-check
 
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import tseslint from 'typescript-eslint';
 
 export default [
-  // Ignore patterns
-  {
-    ignores: ["dist/**", "node_modules/**"],
-  },
-
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-
-  {
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        process: "readonly",
-        console: "readonly",
-        __dirname: "readonly",
-        __filename: "readonly",
-      },
+    // Ignore patterns
+    {
+        ignores: ['dist/**', 'node_modules/**']
     },
-    files: ["**/*.ts"],
-    rules: {
-      "no-console": "error",
-      "no-unused-vars": "off", // Turn off base rule
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
+
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintConfigPrettier,
+
+    {
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                process: 'readonly',
+                console: 'readonly',
+                __dirname: 'readonly',
+                __filename: 'readonly'
+            }
         },
-      ],
-      "@typescript-eslint/no-explicit-any": "warn",
-      "@typescript-eslint/no-var-requires": "off",
-    },
-  },
+        files: ['**/*.ts'],
+        rules: {
+            'no-console': 'error',
+            'no-unused-vars': 'off', // Turn off base rule
+            '@typescript-eslint/no-unused-vars': [
+                'warn',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_'
+                }
+            ],
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/no-var-requires': 'off'
+        }
+    }
 ];
+
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,4 @@
-
+let flag = true;
+if (flag) {
+    flag = false;
+}
